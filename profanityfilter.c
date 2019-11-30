4cafc67a0a2f5d86c8bae83add6f8ee06f2ad601
TinsonWang <tinsonwang@gmail.com>
2019-11-30 15:16:06 -0500

    Revised starting files for project

---
+++ profanityfilter.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    FILE* filter;
+    FILE* text;
+
+    if ( (filter = fopen("list.txt", "r") != NULL) )
+    {
+        printf("Filter successfully loaded!\n");
+    }
+    else
+    {
+        printf("ERROR: Could not load in filter!\n");
+    }
+
+    if ( (text = fopen(argv[1], "r") != NULL) )
+    {
+        printf("Text successfully loaded!\n");
+    }
+    else
+    {
+        printf("ERROR: Could not load in text!\n");
+    }
+
+    return 0;
+    
+}
