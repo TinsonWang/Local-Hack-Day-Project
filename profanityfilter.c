1bb23294fb9382c91b9c7800b7fa766563937214
T M Shamsi Shinin <tshinin@uoguelph.ca>
2019-11-30 16:00:00 -0500

    File Loading

--- profanityfilter.c
+++ profanityfilter.c
@@ -1,28 +1,61 @@
 #include <stdio.h>
 
+#define MAXSIZE 999
+
 int main(int argc, char* argv[])
 {
-    FILE* filter;
-    FILE* text;
+    FILE* f1;
+    FILE* f2;
+    FILE* f3;
+    char badLog[MAXSIZE][MAXSIZE];
+    char goodLog[MAXSIZE][MAXSIZE];
+    char textLog[MAXSIZE][MAXSIZE];
+    int i = 0;
 
-    if ( (filter = fopen("list.txt", "r") != NULL) )
+
+    if ( (f1 = fopen("badWords.txt", "r")) != NULL )
     {
-        printf("Filter successfully loaded!\n");
+	    while (fgets(badLog[i], MAXSIZE, f1) != NULL) 
+	    {
+		    i++;
+	    }
+
+    } else 
+    {
+	    printf ("ERROR: File cannot be opened\n");
+	    exit(1);
     }
-    else
+    i = 0;
+
+    if ( (f2 = fopen("goodWords.txt", "r")) != NULL )
     {
-        printf("ERROR: Could not load in filter!\n");
+	    while (fgets(goodLog[i], MAXSIZE, f2) != NULL) 
+	    {
+		    i++;
+	    }
+
+    }else 
+    {
+	    printf ("ERROR: File cannot be opened\n");
+	    exit(1);
     }
 
-    if ( (text = fopen(argv[1], "r") != NULL) )
+    i = 0;
+
+    if ( (f3 = fopen("text.txt", "r")) != NULL )
     {
-        printf("Text successfully loaded!\n");
-    }
-    else
+	    while (fgets(textLog[i], MAXSIZE, f3) != NULL) 
+	    {
+		    i++;
+	    }
+
+    } else 
     {
-        printf("ERROR: Could not load in text!\n");
+	    printf ("ERROR: File cannot be opened\n");
+	    exit(1);
     }
 
+    
     return 0;
     
 }
